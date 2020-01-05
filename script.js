60f72374f2ea208be04696a18a5a511d97a21e67
peterkrumb <petekrumb@gmail.com>
2020-01-04 21:47:34 -0600

    render temperature, humidity, city name to page

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+var date = moment().format("L");
+
+$("#add-city").on("click", function(event) {
+    event.preventDefault();
+    getCurrentWeather();
+});
+
 function getCurrentWeather() {
-    getFiveDay(city);
-  
-    //For Todays Weather
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d826ebf2a38a534fcc03caba12e863aa";
+    var cityInput = $("#city-input").val();
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=d826ebf2a38a534fcc03caba12e863aa";  
     $.ajax({
       url: queryURL,
       method: "GET"
-    })
-      .then(function(response) {
+    }).then(function(response) {
         console.log(response);
+        $(".city-name").html(response.name + " (" + date + ") ");
+        $(".weather-display").attr("src", "http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+        $(".temp").html("Temperature: " + ((response.main.temp - 273.15) * 1.8 + 32).toFixed() + "&#176;F");
+        $(".humidity").text("Humidity: " + response.main.humidity + "%");
+        $(".wind").html("Wind Speed: " + response.wind.speed + " MPH");
+       })
   }
